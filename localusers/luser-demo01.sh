114b79e16995d575c7d922751c882facc3bb8cb8
Rudramani4493 <rdubey4493@gmail.com>
2022-08-14 20:50:01 +0530

    initial commit

---
+++ localusers/luser-demo01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script displays various information on the screen.
+
+echo 'Hello'
+# Assigns value to a variable
+WORD='script'
+echo "$WORD"
+
+# Hard code variable with twxts
+
+echo "This is a variable having value $WORD"
